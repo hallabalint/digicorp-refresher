6071300476232dbd97a81187992751e0a38c49b2
hallabalint <59656917+hallabalint@users.noreply.github.com>
2024-05-11 08:06:28 +0200

    v2

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+p = document.createElement("div");
+p.innerHTML = '<label for="interval">frissítés</label><input type="number" id="interval" value="60"><label for="scroll">Auro scroll</label><input type="checkbox" id="scroll">';
+p.style = 'position: -webkit-sticky;position: sticky;top: 0; border: 1px solid black; z-index:999;background-color: white;';
+document.body.insertBefore(p, document.body.firstChild);
+
 function refresh() {
     const races = document.getElementsByClassName("linked");
-    Array.prototype.forEach.call(races, function(element) {
+    Array.prototype.forEach.call(races, function (element) {
         if (element.getAttribute("closed") != 'true') {
             let id = element.childNodes[1].childNodes[1].innerText;
             let request = new XMLHttpRequest();
+            let server = window.location.hostname;
             //request https://results.szeged2024.com/broadcast/competition/1/futamstatusz/+id
-            request.open('GET', 'https://results.szeged2024.com/broadcast/competition/1/futamstatusz/' + id, true);
-            //get anwser as a string
+            request.open('GET', 'https://' + server + '/broadcast/competition/1/futamstatusz/' + id, true);
             request.onload = function () {
                 let data = request.responseText;
                 console.log(data);
-                //remove \r \n
                 data = data.replace(/(\r\n|\n|\r)/gm, "");
                 data = data.split(',');
                 if (data[1] == 'official') {
@@ -21,12 +25,19 @@ function refresh() {
                 if (data[1] == 'unofficial') {
                     element.style.backgroundColor = "#99ccff";
                 }
-                let closed = document.getElementsByClassName("closed");
-                closed[closed.length-1].scrollIntoView(true);
+                if (document.getElementById("scroll").checked) {
+                    let closed = document.getElementsByClassName("closed");
+                    closed[closed.length - 1].scrollIntoView({ block: "center" });
+                }
             }
             request.send();
         }
     });
-    
+
 }
-setInterval(refresh, 60000);
+let updater = setInterval(refresh, 60000);
+document.getElementById("interval").onchange = function () {
+    clearInterval(updater);
+    updater = setInterval(refresh, this.value * 1000);
+}
+refresh();
