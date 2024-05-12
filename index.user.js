b0c3b45ce9cbd3bf8569beed70c98cc5124ba564
hallabalint <59656917+hallabalint@users.noreply.github.com>
2024-05-12 22:53:30 +0200

    v1.0 (tamper update)

--- index.user.js
+++ index.user.js
@@ -1,58 +1,58 @@
 // ==UserScript==
 // @name         Digicorp Refresher
 // @namespace    http://hbj.hu/
-// @version      v1.0
+// @version      v1.1
 // @description  Auto refresh and scroll for digicorp results page
 // @downloadURL  https://github.com/hallabalint/digicorp-refresher/releases/latest/download/index.tamper.js
-// @author       You
+// @author       github.com/hallabalint
 // @match        https://results.szeged2024.com/results/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     let p = document.createElement("div");
-p.innerHTML = '<label for="interval">Refresh interval (s)</label><input type="number" id="interval" value="60"><label for="scroll">Auto scroll</label><input type="checkbox" id="scroll"><input type="button" value="Refresh" onClick="refresh();">';
-p.style = 'position: -webkit-sticky;position: sticky;top: 0; border: 1px solid black; z-index:999;background-color: white;';
-document.body.insertBefore(p, document.body.firstChild);
+    p.innerHTML = '<label for="interval">Refresh interval (s)</label><input type="number" id="interval" value="60"><label for="scroll">Auto scroll</label><input type="checkbox" id="scroll"><input type="button" value="Refresh" onClick="refresh();">';
+    p.style = 'position: -webkit-sticky;position: sticky;top: 0; border: 1px solid black; z-index:999;background-color: white;';
+    document.body.insertBefore(p, document.body.firstChild);
 
-function refresh() {
-    const races = document.getElementsByClassName("linked");
-    Array.prototype.forEach.call(races, function (element) {
-        if (element.getAttribute("closed") != 'true') {
-            let id = element.childNodes[1].childNodes[1].innerText;
-            let request = new XMLHttpRequest();
-            let server = window.location.hostname;
-            request.open('GET', 'https://' + server + '/broadcast/competition/1/futamstatusz/' + id, true);
-            request.onload = function () {
-                let data = request.responseText;
-                console.log(data);
-                data = data.replace(/(\r\n|\n|\r)/gm, "");
-                data = data.split(',');
-                if (data[1] == 'official') {
-                    element.setAttribute("closed", "true");
-                    element.classList.add("closed");
-                    element.style.backgroundColor = "#99ffcc";
+    function refresh() {
+        const races = document.getElementsByClassName("linked");
+        Array.prototype.forEach.call(races, function (element) {
+            if (element.getAttribute("closed") != 'true') {
+                let id = element.childNodes[1].childNodes[1].innerText;
+                let request = new XMLHttpRequest();
+                let server = window.location.hostname;
+                request.open('GET', 'https://' + server + '/broadcast/competition/1/futamstatusz/' + id, true);
+                request.onload = function () {
+                    let data = request.responseText;
+                    console.log(data);
+                    data = data.replace(/(\r\n|\n|\r)/gm, "");
+                    data = data.split(',');
+                    if (data[1] == 'official') {
+                        element.setAttribute("closed", "true");
+                        element.classList.add("closed");
+                        element.style.backgroundColor = "#99ffcc";
+                    }
+                    if (data[1] == 'unofficial') {
+                        element.style.backgroundColor = "#99ccff";
+                    }
+                    if (document.getElementById("scroll").checked) {
+                        let closed = document.getElementsByClassName("closed");
+                        closed[closed.length - 1].scrollIntoView({ block: "center" });
+                    }
                 }
-                if (data[1] == 'unofficial') {
-                    element.style.backgroundColor = "#99ccff";
-                }
-                if (document.getElementById("scroll").checked) {
-                    let closed = document.getElementsByClassName("closed");
-                    closed[closed.length - 1].scrollIntoView({ block: "center" });
-                }
+                request.send();
             }
-            request.send();
-        }
-    });
+        });
 
-}
-let updater = setInterval(refresh, 60000);
-document.getElementById("interval").onchange = function () {
-    clearInterval(updater);
-    updater = setInterval(refresh, this.value * 1000);
-}
-refresh();
+    }
+    let updater = setInterval(refresh, 60000);
+    document.getElementById("interval").onchange = function () {
+        clearInterval(updater);
+        updater = setInterval(refresh, this.value * 1000);
+    }
+    refresh();
 
 })();
